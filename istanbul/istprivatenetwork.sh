5faaf73201f427fe546b74b6e315cec28ce2c63d
aneeque.safdar <as@clearmatics.com>
2018-08-13 17:41:12 +0100

    Added support of launching clique poa network

--- istanbul/istprivatenetwork.sh
+++ istanbul/istprivatenetwork.sh
@@ -2,18 +2,19 @@
 
 # Define variables
 ARGS_LENGTH=${#@}
-STARTING_DIR=`pwd`
-ISTANBUL_DIR="${STARTING_DIR}/istanbultestnet"
-PASSWORD_PATH="${ISTANBUL_DIR}/passwd.txt"
-NUMBER_OF_NODES=$2
+STARTING_DIR=`dirname \`readlink -f $0\``
+NETWORK_DIR="${STARTING_DIR}/poaoribftnetwork"
+PASSWORD_PATH="${NETWORK_DIR}/passwd.txt"
 GETH_BIN_PATH=$1
+NUMBER_OF_NODES=$2
+CONSENSUS_ALGO=$3
 ISTANBUL_TOOLS_PATH="${GOPATH}/src/github.com/getamis/istanbul-tools"
 ISTANBUL_TOOLS_GITHUB="https://github.com/getamis/istanbul-tools.git"
-TMUX_SESSION_NAME="istanbul_network"
+TMUX_SESSION_NAME="poaoribft_network"
 BOOTNODE_PORT=4800
 PORT=4800
 RPC_PORT=9501
-CHAINID=1530
+CHAINID=2017
 
 # Create a common password file
 createPasswd() {
@@ -41,10 +42,10 @@ createAccounts() {
     for i in `seq 1 "${NUMBER_OF_NODES}"`
     do
     echo
-        if ! [ -d  ""${ISTANBUL_DIR}/node${i}"" ]; then
-            mkdir -p ""${ISTANBUL_DIR}/node${i}""
+        if ! [ -d  ""${NETWORK_DIR}/node${i}"" ]; then
+            mkdir -p ""${NETWORK_DIR}/node${i}""
         fi
-        ${GETH_BIN_PATH} --datadir "${ISTANBUL_DIR}/node${i}" --password ${PASSWORD_PATH} account new
+        ${GETH_BIN_PATH} --datadir "${NETWORK_DIR}/node${i}" --password ${PASSWORD_PATH} account new
     echo
     done
     echo "---------Finished creating Accounts---------"
@@ -55,23 +56,23 @@ createToml() {
     VANITY="0x00"
     NODE_ADDRESSES=("$@")
 
-    if [ -f "${ISTANBUL_DIR}/config.toml" ]; then
-        rm "${ISTANBUL_DIR}/config.toml"
+    if [ -f "${NETWORK_DIR}/config.toml" ]; then
+        rm "${NETWORK_DIR}/config.toml"
     fi
 
-    echo "vanity = \"${VANITY}\"" >> "${ISTANBUL_DIR}/config.toml"
+    echo "vanity = \"${VANITY}\"" >> "${NETWORK_DIR}/config.toml"
 
     count=1
     for i in ${NODE_ADDRESSES[@]}
     do
         if [ ${#NODE_ADDRESSES[@]} -eq "1" ]; then
-            echo "validators = [\"0x${i}\"]" >> "${ISTANBUL_DIR}/config.toml"
+            echo "validators = [\"0x${i}\"]" >> "${NETWORK_DIR}/config.toml"
         elif [ ${count} -eq "1" ]; then
-            echo "validators = [\"0x${i}\"," >> "${ISTANBUL_DIR}/config.toml"
+            echo "validators = [\"0x${i}\"," >> "${NETWORK_DIR}/config.toml"
         elif [ ${count} -eq ${#NODE_ADDRESSES[@]} ]; then
-            echo "${string}\"0x${i}\"]" >> "${ISTANBUL_DIR}/config.toml"
+            echo "${string}\"0x${i}\"]" >> "${NETWORK_DIR}/config.toml"
         else
-            echo "${string}\"0x${i}\"," >> "${ISTANBUL_DIR}/config.toml"
+            echo "${string}\"0x${i}\"," >> "${NETWORK_DIR}/config.toml"
         fi
         count=`expr ${count} + 1`
     done
@@ -83,29 +84,31 @@ createGenesis() {
     echo "---------Creating Genesis file---------"
     # Get the address from keystore and store them in an array
     ADDRESSES=()
-    for i in `ls ${ISTANBUL_DIR} | grep node`
+    for i in `ls ${NETWORK_DIR} | grep node`
     do
-        ADDRESSES+=(`ls ${ISTANBUL_DIR}/${i}/keystore | cut -d'-' -f9`)
+        ADDRESSES+=(`ls ${NETWORK_DIR}/${i}/keystore | cut -d'-' -f9`)
     done
 
-    # Clone istanbul-tools
-    if ! [ -d "${ISTANBUL_TOOLS_PATH}" ]; then
-        git clone "${ISTANBUL_TOOLS_GITHUB}" "${ISTANBUL_TOOLS_PATH}"
+    if [ ${CONSENSUS_ALGO} = "istanbul" ]; then
+        # Clone istanbul-tools
+        if ! [ -d "${ISTANBUL_TOOLS_PATH}" ]; then
+            git clone "${ISTANBUL_TOOLS_GITHUB}" "${ISTANBUL_TOOLS_PATH}"
+        fi
+
+        # Make istanbul-tools
+        if ! [ -f "${ISTANBUL_TOOLS_PATH}/build/bin/istanbul" ]; then
+            make -C ${ISTANBUL_TOOLS_PATH}
+        fi
+
+        # Create config.toml with the validators
+        createToml ${ADDRESSES[@]}
+
+        # Call istanbul extra encode and store in a file
+        ${ISTANBUL_TOOLS_PATH}/build/bin/istanbul extra encode --config "${NETWORK_DIR}/config.toml" | cut -d':' -f2 | tr -d " \t\n\r" >  "${NETWORK_DIR}/newextradata.txt"
     fi
-
-    # Make istanbul-tools
-    if ! [ -f "${ISTANBUL_TOOLS_PATH}/build/bin/istanbul" ]; then
-        make -C ${ISTANBUL_TOOLS_PATH}
-    fi
-
-    # Create config.toml with the validators
-    createToml ${ADDRESSES[@]}
-
-    # Call istanbul extra encode and store in a file
-    ${ISTANBUL_TOOLS_PATH}/build/bin/istanbul extra encode --config "${ISTANBUL_DIR}/config.toml" | cut -d':' -f2 | tr -d " \t\n\r" >  "${ISTANBUL_DIR}/newextradata.txt"
 
     # Update genesis file, the genesis.json and extra data's file location is passed in and the rest of the addresses are passed to assign some ether
-    node "${STARTING_DIR}/updategenesis.js" "${ISTANBUL_DIR}/genesis.json" "${ISTANBUL_DIR}/newextradata.txt" ${CHAINID} ${ADDRESSES[@]}
+    node "${STARTING_DIR}/updategenesis.js" ${NETWORK_DIR} ${CHAINID} ${CONSENSUS_ALGO} ${ADDRESSES[@]}
     echo "---------Finished creating Genesis file---------"
     echo
 }
@@ -113,10 +116,10 @@ createGenesis() {
 initialiseNodes() {
     echo
     echo "---------Initialising nodes with Genesis file---------"
-    for i in `ls ${ISTANBUL_DIR} | grep node`
+    for i in `ls ${NETWORK_DIR} | grep node`
     do
     echo
-        ${GETH_BIN_PATH} --datadir "${ISTANBUL_DIR}/${i}" init "${ISTANBUL_DIR}/genesis.json"
+        ${GETH_BIN_PATH} --datadir "${NETWORK_DIR}/${i}" init "${NETWORK_DIR}/genesis.json"
     echo
     done
     echo "---------Finished initialising nodes with Genesis file---------"
@@ -125,24 +128,24 @@ initialiseNodes() {
 
 
 createBootNodeKey() {
-    bootnode -genkey "${ISTANBUL_DIR}/boot.key"
+    bootnode -genkey "${NETWORK_DIR}/boot.key"
 }
 
 launchBootNode() {
     tmux new -s ${TMUX_SESSION_NAME} -n "bootnode" -d
-    tmux send-keys -t "${TMUX_SESSION_NAME}:bootnode" "bootnode -nodekey \"${ISTANBUL_DIR}/boot.key\" -verbosity 9 -addr :${BOOTNODE_PORT}" C-m
+    tmux send-keys -t "${TMUX_SESSION_NAME}:bootnode" "bootnode -nodekey \"${NETWORK_DIR}/boot.key\" -verbosity 9 -addr :${BOOTNODE_PORT}" C-m
     PORT=`expr ${PORT} + 1`
 }
 
 launchNodes() {
     count=1
-    for i in `ls ${ISTANBUL_DIR} | grep node`
+    for i in `ls ${NETWORK_DIR} | grep node`
     do
         tmux new-window -t ${TMUX_SESSION_NAME}:${count} -n ${i} 
-        tmux send-keys -t ${TMUX_SESSION_NAME}:${count} "${GETH_BIN_PATH} --datadir "${ISTANBUL_DIR}/${i}" --syncmode 'full' --port ${PORT} --rpcport ${RPC_PORT} --rpc --rpcaddr '0.0.0.0' --rpccorsdomain '*' --rpcapi 'personal,db,eth,net,web3,txpool,miner,istanbul' --bootnodes 'enode://`bootnode -nodekey ${ISTANBUL_DIR}/boot.key -writeaddress`@127.0.0.1:${BOOTNODE_PORT}' --networkid ${CHAINID} --gasprice '0' -unlock \"0x`ls ${ISTANBUL_DIR}/${i}/keystore | cut -d'-' -f9`\" --password ${PASSWORD_PATH} --debug --mine --minerthreads '1' --etherbase \"0x`ls ${ISTANBUL_DIR}/${i}/keystore | cut -d'-' -f9`\"" C-m
+        tmux send-keys -t ${TMUX_SESSION_NAME}:${count} "${GETH_BIN_PATH} --datadir "${NETWORK_DIR}/${i}" --syncmode 'full' --port ${PORT} --rpcport ${RPC_PORT} --rpc --rpcaddr '0.0.0.0' --rpccorsdomain '*' --rpcapi 'personal,db,eth,net,web3,txpool,miner,istanbul' --bootnodes 'enode://`bootnode -nodekey ${NETWORK_DIR}/boot.key -writeaddress`@127.0.0.1:${BOOTNODE_PORT}' --networkid ${CHAINID} --gasprice '0' -unlock \"0x`ls ${NETWORK_DIR}/${i}/keystore | cut -d'-' -f9`\" --password ${PASSWORD_PATH} --debug --mine --minerthreads '1' --etherbase \"0x`ls ${NETWORK_DIR}/${i}/keystore | cut -d'-' -f9`\"" C-m
         tmux split-window -h -t ${TMUX_SESSION_NAME}:${count}
         tmux send-keys -t ${TMUX_SESSION_NAME}:${count} "sleep 10s" C-m
-        tmux send-keys -t ${TMUX_SESSION_NAME}:${count} "${GETH_BIN_PATH} attach ipc:${ISTANBUL_DIR}/${i}/geth.ipc" C-m
+        tmux send-keys -t ${TMUX_SESSION_NAME}:${count} "${GETH_BIN_PATH} attach ipc:${NETWORK_DIR}/${i}/geth.ipc" C-m
         
         PORT=`expr ${PORT} + 1`
         RPC_PORT=`expr ${RPC_PORT} + 1`
@@ -152,36 +155,42 @@ launchNodes() {
 
 ### Start of the main script
 if [ ${ARGS_LENGTH} -eq "1" ]; then
-    if [ -d ${ISTANBUL_DIR} ]; then
+    if [ -d ${NETWORK_DIR} ]; then
     tmux kill-session -t ${TMUX_SESSION_NAME} > /dev/null 2>&1
         launchBootNode
         launchNodes
     else
-        echo "Please create an istanbul network by passing geth binary with the number of node you would like to create"
+        echo "Please create an istanbul or clique network by passing geth binary with the number of node you would like to create and consensus algorithm"
         exit 1
     fi
-elif [ ${ARGS_LENGTH} -eq "2" ]; then
-    tmux kill-session -t ${TMUX_SESSION_NAME} > /dev/null 2>&1
-    if [ -d "${ISTANBUL_DIR}" ]; then
-        rm -rf "${ISTANBUL_DIR}"
+elif [ ${ARGS_LENGTH} -eq "3" ]; then
+    if [ ${CONSENSUS_ALGO} = "istanbul" ] || [ ${CONSENSUS_ALGO} = "clique" ]; then
+        echo $3
+        tmux kill-session -t ${TMUX_SESSION_NAME} > /dev/null 2>&1
+        if [ -d "${NETWORK_DIR}" ]; then
+            rm -rf "${NETWORK_DIR}"
+        fi
+
+        createDir "${NETWORK_DIR}"
+        cd "${NETWORK_DIR}"
+
+        createPasswd
+
+        createAccounts
+
+        createGenesis
+
+        initialiseNodes
+
+        createBootNodeKey
+
+        launchBootNode
+        launchNodes
+    else
+        echo "Please enter the consensus algorithm you want: \"istanbul\" or \"clique\", as the last argument"
+        exit 1
     fi
-
-    createDir "${ISTANBUL_DIR}"
-    cd "${ISTANBUL_DIR}"
-
-    createPasswd
-
-    createAccounts
-
-    createGenesis
-
-    initialiseNodes
-
-    createBootNodeKey
-
-    launchBootNode
-    launchNodes
 else
-    echo "Please create an istanbul network by passing geth binary with the number of node you would like to create"
+    echo "Please create an istanbul or clique network by passing geth binary with the number of node you would like to create and consensus algorithm"
     exit 1
 fi
--- istanbul/updategenesis.js
+++ istanbul/updategenesis.js
@@ -1,8 +1,10 @@
 const fs = require('fs')
 const commandLineArgs = process.argv.slice(2)
-const genesisFilePath = commandLineArgs[0]
-const newExtraDataFilePath = commandLineArgs[1]
-const newChainId = parseInt(commandLineArgs[2])
+const networkDir = commandLineArgs[0]
+const genesisFilePath = networkDir + "/genesis.json"
+const newExtraDataFilePath = networkDir + "/newextradata.txt"
+const newChainId = parseInt(commandLineArgs[1])
+const consensusAlgorithm = commandLineArgs[2]
 const addresses = commandLineArgs.slice(3)
 
 let genesis = {
@@ -12,31 +14,43 @@ let genesis = {
         "eip150Hash": "0x0000000000000000000000000000000000000000000000000000000000000000",
         "eip155Block": 3,
         "eip158Block": 3,
-        "istanbul": {
-            "epoch": 30000,
-            "policy": 0
-        }
     },
     "nonce": "0x0",
     "timestamp": "0x0",
     "gasLimit": "0x47b760",
     "difficulty": "0x1",
-    "mixHash": "0x63746963616c2062797a616e74696e65206661756c7420746f6c6572616e6365",
     "coinbase": "0x0000000000000000000000000000000000000000",
     "number": "0x0",
     "gasUsed": "0x0",
     "parentHash": "0x0000000000000000000000000000000000000000000000000000000000000000"
 }
 
-let newExtraData = fs.readFileSync(newExtraDataFilePath, 'utf8')
-let startingBalance = "0x446c3b15f9926687d2c40534fdb564000000000000"
-
-// Update genesis.extraData
-genesis.extraData = newExtraData
+let startingBalance = "0x200000000000000000000000000000000000000000000000000000000000000"
 genesis.alloc = {}
 for (var i = 0 ; i < addresses.length; i++) {
     genesis.alloc[addresses[i]] = {"balance" : startingBalance}
 }
 genesis.config.chainId = newChainId
+
+if (consensusAlgorithm == "istanbul") {
+    genesis.config["istanbul"] = {
+        "epoch": 30000,
+        "policy": 0
+    }
+    genesis["mixHash"] = "0x63746963616c2062797a616e74696e65206661756c7420746f6c6572616e6365"
+    genesis.extraData = fs.readFileSync(newExtraDataFilePath, 'utf8')
+    
+} else if (consensusAlgorithm == "clique") {
+    genesis.config["clique"] = {
+        "period": 15,
+        "epoch": 30000
+    }
+    genesis.config["byzantiumBlock"] = 4
+    genesis["mixHash"] = "0x0000000000000000000000000000000000000000000000000000000000000000"
+    genesis.extraData = 
+    "0x0000000000000000000000000000000000000000000000000000000000000000" + 
+    addresses.join('') + 
+    "0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"
+}
 
 fs.writeFileSync(genesisFilePath, JSON.stringify(genesis, null, 4))
